0cd077faf399f87c003cfb44b67d4b43950f5393
Bruno <brunoborges2001@gmail.com>
2022-04-30 21:49:54 -0300

    :tada: project done

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+interface HeaderProps {
+  onNewTransactionButtonClick: () => void;
+}
+
+export function Header({ onNewTransactionButtonClick }: HeaderProps) {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onNewTransactionButtonClick}>
+          Nova Transação
+        </button>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,63 @@
+import { useMemo } from "react";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { useTransactions } from "../../hooks/useTransactions";
+import { Container } from "./styles";
+
+export function Summary() {
+  const { transactions } = useTransactions();
+
+  const { income, outcome } = useMemo(
+    () =>
+      transactions.reduce(
+        (total, el) =>
+          el.type === "deposit"
+            ? { ...total, income: total.income + el.amount }
+            : { ...total, outcome: total.outcome + el.amount },
+        { income: 0, outcome: 0 }
+      ),
+    [transactions]
+  );
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(income)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(outcome)}
+        </strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(income - outcome)}
+        </strong>
+      </div>
+    </Container>
+  );
+}
